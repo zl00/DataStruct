9f0b15df2f8d6b4f70271e3e116a376d2bd0027e
Linda.Zhong <wm80098@worksmobile.com>
2019-01-05 13:52:45 +0800

    ddd

--- Algorithm.playground/Pages/Sort-Select-Heap.xcplaygroundpage/Contents.swift
+++ Algorithm.playground/Pages/Sort-Select-Heap.xcplaygroundpage/Contents.swift
@@ -3,54 +3,46 @@
 import Foundation
 
 // [wiki](https://www.cnblogs.com/chengxiao/p/6129630.html)
-// 构造大顶堆
 
-func HEAP(_ result: inout [Int], target: Int) {
-    guard target >= 1 else { return }
+func swap(_ result: inout [Int], i: Int, j: Int) {
+    let tmp = result[i]
+    result[i] = result[j]
+    result[j] = tmp
+}
 
-    let left = 2*target+1, right = 2*target+2
-    var isLeft = false, isRight = false
+func sift(_ result: inout [Int], low: Int, high: Int) {
+    var i = low
+    var j = 2 * i + 1 // left
     
-    if left < result.count, result[target] < result[left] {
-        let tmp = result[target]
-        result[target] = result[left]
-        result[left] = tmp
-        isLeft = true
-    }
-    
-    if right < result.count, result[target] < result[right] {
-        let tmp = result[target]
-        result[target] = result[right]
-        result[right] = tmp
-        isRight = true
-    }
-    
-    if isLeft && isRight {
-        HEAP(&result, target: left)
-        HEAP(&result, target: right)
+    while j <= high {
+        if j + 1 <= high, result[j] < result[j+1] { j += 1 } // fetch the bigger leaf
+        
+        guard result[i] < result[j] else { break }
+        
+        swap(&result, i: i, j: j)
+        
+        i = j
+        j = 2 * i + 1
     }
 }
 
-func HeapSort(_ input: [Int]) -> [Int] {
-    var result = input
-    
-    var count = input.count
-    var target = count/2-1
-    
-    while count >= 2 {
-        HEAP(&result, target: target)
-        
-        let max = result[0]
-        result[0] = result[count-1]
-        result[count-1] = max
-        
-        count -= 1
-        target = count/2-1
+func HeapSort(_ result: inout [Int]) {
+    // 建立堆
+    var low = result.count / 2 - 1
+    while low >= 0 {
+        sift(&result, low: low, high: result.count - 1)
+        low -= 1
     }
     
-    return result
+    var i = result.count - 1
+    while i > 0 {
+        swap(&result, i: 0, j: i) // heap[0]: 最大的数，将其交换到heap的尾部。
+        sift(&result, low: 0, high: i-1) // 此时的堆除了[0]，其余都是排序好的。所以只需要sift一轮即可。
+        i -= 1
+    }
 }
 
+var arr = [49, 38, 65, 97, 13, 1, 129, 12]
+HeapSort(&arr)
+print(arr)
 
-
-print(HeapSort([49, 38, 65, 97, 13, 1, 129, 12]))
--- 排序/3-SelectSort-Heap.c
+++ 排序/3-SelectSort-Heap.c
@@ -1,19 +1,34 @@
 void Sift(int R[], int low, int high) {
-    int i = low, j = 2*i;
+    int i = low, j = 2 * i;
     int temp = R[i];
+
     while (j <= high) {
         if (j < high && R[j] < R[j+1]) ++j;
 
         if (temp < R[j]) {
-
+            R[i] = R[j];
+            i = j;
+            j = 2 * i;
+        } else {
+            break;
         }
+
+        R[i] = temp;
     }
 }
 
 void heapSort(int R[], int n) {
     int i;
     int temp;
+    
+    for (i = n/2; i >= 1; --i) {
+        Sift(R, i, n);
+    }
 
-    for (i = n/2; i >=1; --i)
-        
+    for (i = n; i >= 2; --i) {
+        temp = R[1];
+        R[1] = R[i];
+        R[i] = temp;
+        Sift(R, 1, i-1);
+    }
 }
