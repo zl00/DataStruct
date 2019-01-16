2800a3c2cbd096b970b2d40051642caf1c4ed820
Linda.Zhong <wm80098@worksmobile.com>
2019-01-16 14:22:07 +0800

    [ONEAPPMAC-1101]

--- Algorithm.playground/Pages/Sort-Merge.xcplaygroundpage/Contents.swift
+++ Algorithm.playground/Pages/Sort-Merge.xcplaygroundpage/Contents.swift
@@ -2,6 +2,51 @@
 
 import Foundation
 
-var str = "Hello, playground"
+func mergeSort(_ input: inout [Int], _ low: Int, _ high: Int) {
+    guard low < high else { return }
+    
+    let mid = (low + high) / 2
+    
+    mergeSort(&input, low, mid)
+    mergeSort(&input, mid + 1, high)
+    merge(&input, low, mid, high)
+}
 
-//: [Next](@next)
+func merge(_ input: inout [Int], _ low: Int, _ mid: Int, _ high: Int) {
+    var i = low, j = mid + 1
+    var temp = [Int]()
+    
+    // prior to append the smaller ones
+    while i <= mid, j <= high {
+        if input[i] < input[j] {
+            temp.append(input[i])
+            i += 1
+        } else {
+            temp.append(input[j])
+            j += 1
+        }
+    }
+    
+    // handle remains
+    while i <= mid {
+        temp.append(input[i])
+        i += 1
+    }
+    
+    // handle remains
+    while j <= high {
+        temp.append(input[j])
+        j += 1
+    }
+    
+    // rewrite into `input` array
+    var k = low
+    while k <= high {
+        input[k] = temp[k-low]
+        k += 1
+    }
+}
+
+var arr = [49, 38, 65, 97, 13, 1, 129, 12, 132, 12, 38, 13]
+mergeSort(&arr, 0, arr.count-1)
+print(arr)
--- 排序/4-MergeSort.c
+++ 排序/4-MergeSort.c
@@ -8,3 +8,26 @@ void mergeSort(int A[], int low, int high) {
         merge(A, low, mid, high);
     }
 }
+
+void merge(int A[], int low, int mid, int high) {
+    int i = low, j = mid + 1, k = 0;
+    int arr[high+1];
+
+    while (i <= mid && j <= high) {
+        if (A[i] < A[j]) {
+            arr[k++] = A[i];
+            i++;
+        } else {
+            arr[k++] = A[j];
+            j++;
+        }
+    }
+
+    while (i <= mid) {
+        arr[k++] = A[i++];
+    }
+
+    while (j <= high) {
+        arr[k++] = A[j++];
+    }
+}
