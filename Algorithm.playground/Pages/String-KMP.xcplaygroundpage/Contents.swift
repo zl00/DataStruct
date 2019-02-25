daae59bb787518d581987e08bf663c142d7a69b4
Linda.Zhong <wm80098@worksmobile.com>
2019-02-25 17:28:59 +0800

    kmp

--- Algorithm.playground/Pages/String-KMP.xcplaygroundpage/Contents.swift
+++ Algorithm.playground/Pages/String-KMP.xcplaygroundpage/Contents.swift
@@ -4,4 +4,52 @@ import Foundation
 
 var str = "Hello, playground"
 
-//: [Next](@next)
+// MARK: - Util
+extension String {
+    subscript(index: Int) -> unichar {
+       return (self as NSString).character(at: index)
+    }
+}
+
+// MARK: - Sub string
+extension String {
+    var next: [Int] {
+        guard self.count > 0 else { return [Int]() }
+        
+        var i = 0, j = -1
+        var next = [Int](repeating: 0, count: self.count)
+        next[i] = j
+        
+        while i < self.count - 1 {
+            if j == -1 || self[i] == self[j] {
+                i += 1
+                j += 1
+                next[i] = j
+            } else {
+                j = next[j]
+            }
+        }
+        
+        return next
+    }
+}
+
+// MARK: - Primary string
+extension String {
+    func search(_ sub: String) -> Bool {
+        var i = 0, j = 0
+
+        while i < self.count && j < sub.count {
+            if j == -1 || self[i] == sub[j] {
+                i += 1
+                j += 1
+            } else {
+                j = sub.next[j]
+            }
+        }
+
+        return j == sub.count
+    }
+}
+
+"abcdadadaaddadcaadadadadadasdadasdadsadsadadaaddaa".search("111")
