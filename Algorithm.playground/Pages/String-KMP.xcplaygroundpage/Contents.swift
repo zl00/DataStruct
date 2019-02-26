432613450fcdb1d0f1424fdc454cde8bbe56ed3b
Linda.Zhong <wm80098@worksmobile.com>
2019-02-26 14:25:09 +0800

    kmp: nextval

--- Algorithm.playground/Pages/String-KMP.xcplaygroundpage/Contents.swift
+++ Algorithm.playground/Pages/String-KMP.xcplaygroundpage/Contents.swift
@@ -32,19 +32,43 @@ extension String {
         
         return next
     }
+    
+    var nextVal: [Int] {
+        guard self.count > 0 else { return [Int]() }
+        
+        var i = 0, j = -1
+        var nextval = [Int](repeating: 0, count: self.count)
+        nextval[i] = j
+        
+        while i < self.count - 1 {
+            if j == -1 || self[i] == self[j] {
+                i += 1
+                j += 1
+                
+                if self[i] != self[j] {
+                    nextval[i] = j
+                } else {
+                    nextval[i] = nextval[j]
+                }
+            } else {
+                j = nextval[j]
+            }
+        }
+        return nextval
+    }
 }
 
 // MARK: - Primary string
 extension String {
-    func search(_ sub: String) -> Bool {
+    func search(_ sub: String, _ next: [Int]) -> Bool {
         var i = 0, j = 0
-
+        
         while i < self.count && j < sub.count {
             if j == -1 || self[i] == sub[j] {
                 i += 1
                 j += 1
             } else {
-                j = sub.next[j]
+                j = next[j]
             }
         }
 
@@ -52,4 +76,8 @@ extension String {
     }
 }
 
-"abcdadadaaddadcaadadadadadasdadasdadsadsadadaaddaa".search("111")
+let primary = "abcdadadaaddadcaadadadadadasdadasdadsadsadadaaddaa"
+let sub = "aaaabc"
+
+//primary.search(sub, sub.next)
+primary.search(sub, sub.nextVal)
